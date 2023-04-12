2b631c70b954c65824a5515a58925fe1db33ef31
Calvin Wilkinson <kinsondigital@gmail.com>
2023-04-13 00:19:44 +0100

    cleanup: remove comment in HelloVelaptor sample project

--- HelloVelaptor/Program.cs
+++ HelloVelaptor/Program.cs
@@ -3,12 +3,4 @@
 // </copyright>
 
 var game = new HelloVelaptor.Game();
-
-/* Show the game window.  This starts the game loop.
- * Any code below this point will not run until the Show() method has finished.
- * This is because the Show() method will not completed until the windows is closed.
- *
- * 💡You can use the ShowAsync() method if you want to show the window in an async
- * fashion so you can perform other processes while the window is loading.
-*/
 game.Show();
